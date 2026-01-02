a86943f09f1f8b32c58172cc1b27fe5b62d2dd2e
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-01-02 11:50:17 +0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const app = require("./src/index");
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("🚀 Server running on port", PORT);
-});
