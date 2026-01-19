f7bc3068e8b325712a57800cc8613802a0e86e21
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-01-19 14:30:58 +0500

    Update purchase.js

--- routes/purchase.js
+++ routes/purchase.js
@@ -350,7 +350,7 @@ router.post("/save", async (req, res) => {
       await db.query(
         `
         INSERT INTO purchase_entries (
-          ref_no, item, item_label,
+          ref_no, item,
           sale_sar, sale_rate, sale_pkr,
           purchase_sar, purchase_rate, purchase_pkr,
           profit,
@@ -358,8 +358,8 @@ router.post("/save", async (req, res) => {
           supplier_name,
           is_deleted
         )
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,false)
-        ON CONFLICT (ref_no, item_label)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,false)
+        ON CONFLICT (ref_no, item)
         DO UPDATE SET
           sale_sar       = EXCLUDED.sale_sar,
           sale_rate      = EXCLUDED.sale_rate,
@@ -375,7 +375,6 @@ router.post("/save", async (req, res) => {
         [
           ref_no,
           r.item,
-          r.item_label || r.item,
           r.sale_sar || 0,
           r.sale_rate || 0,
           r.sale_pkr || 0,
@@ -384,7 +383,7 @@ router.post("/save", async (req, res) => {
           r.purchase_pkr || 0,
           r.profit || 0,
           r.supplier_code || "",
-          r.supplier_name || ""
+          r.supplier_name || "",
         ]
       );
     }
@@ -708,4 +707,3 @@ router.get("/pending", async (req, res) => {
 
 
 module.exports = router;
-
