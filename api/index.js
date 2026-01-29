98567e8bd47e1324711b75f1ce5d3655619d67de
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-01-29 14:12:15 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -100,6 +100,9 @@ app.use("/api/purchase", require("../routes/purchase"));
 app.use("/supplier", require("../routes/supplier"));
 app.use("/api/supplier", require("../routes/supplier"));
 
+app.use("/next", require("./routes/next"));
+app.use("/api/next", require("./routes/next"));
+
 
 // ==========================
 // AUTH
