5bebe1ba8c0fafb4baa4f6f2eaf8cb9fba5ab961
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-07-06 16:35:42 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,11 +5,7 @@ require("../db"); // ✅ ROOT db.js (Vercel FIX)
 
 const app = express();
 
-app.use(cors({
-  origin: ["https://makki-madni-travel-software.vercel.app", "http://localhost:5173"], // Apne frontend ka sahi URL lagayein.
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // ==========================
