9e16282e2ae2dc81672ca1ce53175ef085325589
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-08-03 13:29:47 +0500

    Update registeredLedger.js

--- routes/registeredLedger.js
+++ routes/registeredLedger.js
@@ -129,7 +129,7 @@ router.get("/detail/:customer_code", async (req, res) => {
     });
 
     // Sort entries chronologically
-    allEntries.sort((a, b) => new Date(a.date) - new Date(b.date));
+    allEntries.sort((a, b) => new Date(b.date) - new Date(a.date));
 
     let balance = 0;
     let filteredRows = [];
